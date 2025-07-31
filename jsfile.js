521d44d932d98030e7ab6f37f7bf31a26ecfdbf5
pavantn36918 <148556331+pavantn36918@users.noreply.github.com>
2025-07-31 22:32:21 +0530

    Updated jsfile.js

--- jsfile.js
+++ jsfile.js
@@ -36,3 +36,59 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+
+// Changes
+
+
+const texts = ["Automation Tester", "Frontend Developer"];
+const h3 = document.querySelector(".text-animate h3");
+const h3After = window.getComputedStyle(h3, '::after');
+let index = 0;
+
+function updateText() {
+    // Change text
+    h3.textContent = texts[index];
+
+
+
+    // Restart the ::after animation by forcing a reflow
+    h3.classList.remove('restart-typing');
+    void h3.offsetWidth; // trigger reflow
+    h3.classList.add('restart-typing');
+
+    // Move to next text
+    index = (index + 1) % texts.length;
+
+}
+
+
+
+// Start the cycle
+updateText();
+setInterval(updateText, 5000); // 4s animation + 1s buffer
+
+document.oncontextmenu = () => {
+    alert("Can't do right click website");
+    return false
+}
+
+document.onkeydown = e => {
+
+    if (e.key == "F12") {
+        alert("Can't Inspect this website");
+        return false
+    }
+
+    if (e.ctrlKey && e.key == "u") {
+        alert("Can't access the page sourse of this website");
+        return false
+    }
+
+}
+
+
+
+
+
+
